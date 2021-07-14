e4d375ea5347bd63cec1a1123df7ec9e4beb2d00
SanghunOh <otter.oh@gmail.com>
2021-07-14 10:44:48 +0900

    change views

--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ def index(request):
     if path == '/home':
         resultstr = '<h1>여기는 home 입니다.</h1>'
     else :
-        resultstr = '<h1>여기는 main 입니다.</h1>'
+        resultstr = '<h1>여기는 mater 입니다.</h1>'
 
     return HttpResponse(resultstr)
 
