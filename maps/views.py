0d184f99b6905f8bf6776be5ff86cab2c7782bd0
SanghunOh <otter.oh@gmail.com>
2021-07-14 14:37:32 +0900

    apply html

--- maps/views.py
+++ maps/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+import folium
 def home(request):
-    return render(request, template_name='maps/home.html')
+    mf = folium.Map([35.3369, 127.7306], zoom_start=10)
+    mf = mf._repr_html_()
+    first = 'sanghun'
+    result = {'mapfolium': mf, 'f01':first}
+
+    return render(request, template_name='maps/home.html', context=result)
